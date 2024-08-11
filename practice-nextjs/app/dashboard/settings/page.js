bb7ebfa10027176dbfb474cf1526bb187ff6a433
asionsolver <asionsolver274@gmail.com>
2024-08-11 14:17:01 +0600

    create new repository name line next-js-projects and set up nextjs successfully

---
+++ practice-nextjs/app/dashboard/settings/page.js
@@ -0,0 +1,12 @@
+const Settings = () => {
+  return (
+    <div>
+      <h1 className='text-2xl bg-purple-400'>Settings Page</h1>
+      <p className="mt-10">
+        This is the settings page. You can change your settings here.
+      </p>
+    </div>
+  );
+};
+
+export default Settings;
